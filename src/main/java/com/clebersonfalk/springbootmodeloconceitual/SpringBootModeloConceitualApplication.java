823ddd0cd55d3631c47654f1e9cd496c996bbc1a
Cleberson Falk <cleberson.falk@gmail.com>
2019-05-21 13:52:24 -0300

    Criação do Projeto - Primeiro commit

---
+++ src/main/java/com/clebersonfalk/springbootmodeloconceitual/SpringBootModeloConceitualApplication.java
@@ -0,0 +1,13 @@
+package com.clebersonfalk.springbootmodeloconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModeloConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootModeloConceitualApplication.class, args);
+	}
+
+}
