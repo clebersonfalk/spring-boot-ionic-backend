9f278469ac05bdb9bc7b1988104b91e1ef587b05
Cleberson Falk <cleberson.falk@gmail.com>
2019-05-24 00:24:38 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/clebersonfalk/springbootmodeloconceitual/SpringBootModeloConceitualApplication.java
+++ src/main/java/com/clebersonfalk/springbootmodeloconceitual/SpringBootModeloConceitualApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.clebersonfalk.springbootmodeloconceitual.domain.Categoria;
+import com.clebersonfalk.springbootmodeloconceitual.domain.Produto;
 import com.clebersonfalk.springbootmodeloconceitual.repositories.CategoriaRepository;
+import com.clebersonfalk.springbootmodeloconceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringBootModeloConceitualApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootModeloConceitualApplication.class, args);
@@ -27,6 +32,18 @@ public class SpringBootModeloConceitualApplication implements CommandLineRunner 
 		Categoria cat2 = new Categoria(null, "Escritório");
 		Categoria cat3 = new Categoria(null, "Papelaria");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
