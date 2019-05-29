6dcd4f40100921ab0e5c160abfd84cc02b99d96d
Cleberson Falk <cleberson.falk@gmail.com>
2019-05-28 21:26:31 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/clebersonfalk/springbootmodeloconceitual/SpringBootModeloConceitualApplication.java
+++ src/main/java/com/clebersonfalk/springbootmodeloconceitual/SpringBootModeloConceitualApplication.java
@@ -13,6 +13,7 @@ import com.clebersonfalk.springbootmodeloconceitual.domain.Cidade;
 import com.clebersonfalk.springbootmodeloconceitual.domain.Cliente;
 import com.clebersonfalk.springbootmodeloconceitual.domain.Endereco;
 import com.clebersonfalk.springbootmodeloconceitual.domain.Estado;
+import com.clebersonfalk.springbootmodeloconceitual.domain.ItemPedido;
 import com.clebersonfalk.springbootmodeloconceitual.domain.Pagamento;
 import com.clebersonfalk.springbootmodeloconceitual.domain.PagamentoComBoleto;
 import com.clebersonfalk.springbootmodeloconceitual.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.clebersonfalk.springbootmodeloconceitual.repositories.CidadeRepositor
 import com.clebersonfalk.springbootmodeloconceitual.repositories.ClienteRepository;
 import com.clebersonfalk.springbootmodeloconceitual.repositories.EnderecoRepository;
 import com.clebersonfalk.springbootmodeloconceitual.repositories.EstadoRepository;
+import com.clebersonfalk.springbootmodeloconceitual.repositories.ItemPedidoRepository;
 import com.clebersonfalk.springbootmodeloconceitual.repositories.PagamentoRepository;
 import com.clebersonfalk.springbootmodeloconceitual.repositories.PedidoRepository;
 import com.clebersonfalk.springbootmodeloconceitual.repositories.ProdutoRepository;
@@ -34,27 +36,22 @@ public class SpringBootModeloConceitualApplication implements CommandLineRunner 
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootModeloConceitualApplication.class, args);
@@ -121,5 +118,18 @@ public class SpringBootModeloConceitualApplication implements CommandLineRunner 
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip1));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
